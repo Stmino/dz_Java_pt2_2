474a2037b8722af0effe0cd42f4c0bd200aaa551
Andrey <dad@>
2023-03-11 15:05:39 +0300

    исправление prog

--- dz2/Progr.java
+++ dz2/Progr.java
@@ -3,17 +3,19 @@ package dz2;
 
 public class Progr {
     public static void main(String[] args) {
+    Cat cat=  new Cat(null);
+    cat.roar();
 
 Animals[] animals = {new Cat(null), new Dog(null), new Tiger(), 
                   new Wolf(false), new Chicken(), new Stork()};
         for(Animals animal :animals)
-            PrintInfo(animal);     
+                PrintInfo(animal);
+        for(Animals animal :animals)
+                animal.roar();            
     }
 
-    private static void PrintInfo(Animals animals){
-        System.out.println(animals);
-        
-        
+    private static void PrintInfo(Animals animal){
+        System.out.println(animal);            
 
 }
 }
